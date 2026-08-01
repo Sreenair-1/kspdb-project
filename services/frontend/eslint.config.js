8ddac1dc12c8d28d5194449ec32f0a8828229d7e
Sreenair-1 <sreeharinair514@gmail.com>
2026-08-01 18:09:02 +0530

    Fix frontend build tooling

--- services/frontend/eslint.config.js
+++ services/frontend/eslint.config.js
@@ -2,9 +2,11 @@ import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
 
 export default [
   js.configs.recommended,
+  ...tseslint.configs.recommended,
   {
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
